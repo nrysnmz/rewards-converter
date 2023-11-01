3ee43d46fc230b0c34c8f65fc1539e4d3b349fa1
Nuray Korganci <nrysnmz@gmail.com>
2023-11-01 06:19:09 -0500

    version

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,33 @@
 public class RewardValue {
     private double conversionRate; // Conversion rate from cash to miles
     private double cashValue; // Store the cash value
+    private double milesValue; // Store the miles value
 
-    public RewardValue(double conversionRate, double cashValue) {
-        this.conversionRate = conversionRate;
+    // Constructor that accepts cash value
+    public RewardValue(double cashValue) {
         this.cashValue = cashValue;
+        this.conversionRate = 1 / 0.0035; // Conversion rate from cash to miles
+        this.milesValue = cashValue * conversionRate;
     }
 
-    public RewardValue(double cashValue) {
+    // Constructor that accepts miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        this.milesValue = milesValue;
+        this.conversionRate = 0.0035; // Conversion rate from miles to cash
+        if (isMiles) {
+            this.cashValue = milesValue * conversionRate;
+        }
     }
 
     public double getMilesValue() {
-        return cashValue * conversionRate;
+        return milesValue;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 }
+
+
+
+
